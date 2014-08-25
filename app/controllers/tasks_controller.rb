46b2f1d569bafa6dbb8ab5522788ebbdcbdce0c9
Meghan Lindsley & Andrew Akers <student@epicodus.com>
2014-08-25 15:26:46 -0700

    add ability to edit tasks, update them, and add a success page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,15 @@ class TasksController < ApplicationController
     render('tasks/index.html.erb')
   end
 
+  def create
+    @task = Task.create(params[:task])
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/new.html.erb')
+    end
+  end
+
   def show
     @task = Task.find(params[:id])
     render('tasks/show.html.erb')
@@ -12,5 +21,19 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
     render('tasks/new.html.erb')
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render('tasks/edit.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(params[:task])
+      render('tasks/success.html.erb')
+    else
+      render('tasks/edit.html.erb')
+    end
   end
 end
