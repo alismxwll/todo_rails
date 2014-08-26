64a76704519ff772ff635d042953d98b993fe67f
Andrew Robert Dean Akers <student@epicodus.com>
2014-08-26 10:40:19 -0700

    see done and not done tasks on index page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   validates :name, :presence => true
-  scope :done -> {where(done: true)}
-  scope :not_done -> {where(not_done: false)}
+  scope :done, -> {where(done: true)}
+  scope :not_done, -> {where(done: nil)}
 end
